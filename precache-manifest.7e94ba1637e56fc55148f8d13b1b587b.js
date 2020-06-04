0d93f37bd46a738cab007e830c59459fd768b79f
Pah Ferreira <pah.webdev@gmail.com>
2020-06-04 18:38:25 -0300

    Updates

---
+++ precache-manifest.7e94ba1637e56fc55148f8d13b1b587b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3065ea29b235051182cca7b100a0a11f",
+    "url": "/d3_challenge/index.html"
+  },
+  {
+    "revision": "35c984384ff41dc62bcc",
+    "url": "/d3_challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45abe5d6570ca79763c9",
+    "url": "/d3_challenge/static/js/2.a39fe0f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/d3_challenge/static/js/2.a39fe0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c984384ff41dc62bcc",
+    "url": "/d3_challenge/static/js/main.962998ad.chunk.js"
+  },
+  {
+    "revision": "b18c4d080ca45483a752",
+    "url": "/d3_challenge/static/js/runtime-main.38358517.js"
+  }
+]);
